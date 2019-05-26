4b4f1fa22d01455fab8ad380b2cdbecb26c732e4
Anthony C Nguyen <anthony.c.nguyen@ucla.edu>
2019-05-25 23:21:41 -0700

    First cut WWRCP

---
+++ WorldWindRCP/src/worldwindrcp/parts/SamplePart.java
@@ -0,0 +1,110 @@
+package worldwindrcp.parts;
+
+import java.awt.EventQueue;
+import java.util.Arrays;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.di.Persist;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+
+import gov.nasa.worldwind.BasicModel;
+import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+
+import javax.swing.*;
+
+public class SamplePart {
+
+	private TableViewer tableViewer;
+	
+	class SimplestPossibleExample extends JFrame
+	{
+	    public SimplestPossibleExample()
+	    {
+	        WorldWindowGLCanvas wwd = new WorldWindowGLCanvas();
+	        wwd.setPreferredSize(new java.awt.Dimension(1000, 800));
+	        this.getContentPane().add(wwd, java.awt.BorderLayout.CENTER);
+	        wwd.setModel(new BasicModel());
+	    }
+
+	}
+	@Inject
+	private MPart part;
+
+	@PostConstruct
+	public void createComposite(Composite parent) {
+		parent.setLayout(new GridLayout(1, false));
+
+		Text txtInput = new Text(parent, SWT.BORDER);
+		txtInput.setMessage("Enter text to mark part as dirty");
+		txtInput.addModifyListener(e -> part.setDirty(true));
+		txtInput.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		tableViewer = new TableViewer(parent);
+
+		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
+		tableViewer.setInput(createInitialDataModel());
+		tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		System.out.println("Gonna do the invoke later");
+		
+		// Setup AWT container.
+		Composite embeddedContainer = new Composite(parent, SWT.EMBEDDED);
+		java.awt.Frame frame = SWT_AWT.new_Frame(embeddedContainer);
+		java.awt.Panel panel = new java.awt.Panel(new java.awt.BorderLayout());
+		frame.add(panel);
+		
+		// Create World Wind canvas and add it to panel.
+		/*
+		WorldWindowGLCanvas wwd = new WorldWindowGLCanvas();
+		wwd.setModel(new BasicModel());
+		panel.add(wwd, java.awt.BorderLayout.CENTER);
+        */
+		/*
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+        		System.out.println("Insider run");
+                JFrame frame = new SimplestPossibleExample();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+		*/
+        EventQueue.invokeLater(new Runnable() { 
+            public void run() { 
+        		System.out.println("Insider run");
+                JFrame frame = new SimplestPossibleExample();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+           } 
+        });
+	}
+
+	@Focus
+	public void setFocus() {
+		tableViewer.getTable().setFocus();
+	}
+
+	@Persist
+	public void save() {
+		part.setDirty(false);
+	}
+	
+	private List<String> createInitialDataModel() {
+		return Arrays.asList("Sample item 1", "Sample item 2", "Sample item 3", "Sample item 4", "Sample item 5");
+	}
+}
